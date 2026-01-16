680f7429d23d067b19439ce70c00effaad437434
kejee1917 <kejee1917@gmail.com>
2026-01-16 13:42:19 +0900

    network: resource v6

---
+++ src/network/tcp/v6/SessionV6.java
@@ -0,0 +1,63 @@
+package network.tcp.v6;
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static network.tcp.SocketCloseUtil.*;
+import static util.MyLogger.log;
+
+public class SessionV6 implements Runnable {
+
+    private final Socket socket;
+    private final DataInputStream input;
+    private final DataOutputStream output;
+    private final SessionManagerV6 sessionManager;
+    private boolean closed = false;
+
+    public SessionV6(Socket socket, SessionManagerV6 sessionManager) throws IOException {
+        this.socket = socket;
+        this.input = new DataInputStream(socket.getInputStream());
+        this.output = new DataOutputStream(socket.getOutputStream());
+        this.sessionManager = sessionManager;
+        this.sessionManager.add(this);
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            while (true) {
+                String received = input.readUTF();
+                log("client -> server: " + received);
+
+                if(received.equals("exit")) {
+                    break;
+                }
+
+                output.writeUTF(received + " World!");
+                log("server -> client:");
+            }
+
+        } catch (IOException e) {
+            log(e);
+        } finally {
+            sessionManager.remove(this);
+            close();
+        }
+
+    }
+
+    // 세션 종료시, 서버 종료시 동시에 호출될 수 있다. synchronized 붙이기
+    public synchronized void close() {
+        if (closed) {
+            return;
+        }
+        closeAll(socket, input, output);
+        closed = true;
+        log("연결 종료: " + socket);
+    }
+}
+
